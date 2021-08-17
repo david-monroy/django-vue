d950074ca528693e0caf4e129cca08592a57201b
David Monroy <djmonroy.17@est.ucab.edu.ve>
2021-08-16 20:29:50 -0400

    Petición a servidor desde componente Vue

--- djvue/frontend/src/components/Participante/ListParticipante.vue
+++ djvue/frontend/src/components/Participante/ListParticipante.vue
@@ -3,13 +3,50 @@
       <div class="row">
           <div class="col text-left">
               <h2>Listado de participantes</h2>
+                {{participantes}}
+              <div class="col-md-12">
+                  <b-table striped hover
+                   :items="participantes"
+                   :fields="fields">
+
+                  </b-table>
+              </div>
           </div>
       </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+    data() {
+        return {
+            fields: [
+                { key: 'name', label: 'Nombre' },
+                { key: 'lastname', label: 'Apellido' },
+                { key: 'action', label: 'Acción' },
+            ],
+            participantes: []
+        }
+    },
+    methods: {
+
+        getParticipantes(){
+            const path = 'http://localhost:8000/api/v1/participantes/'
+            axios.get(path).then((response) => {
+                this.participantes = response.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        },
+    },
+
+    created(){
+        this.getParticipantes()
+    }
+
 
 }
 </script>
